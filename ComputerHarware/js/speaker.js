3cd3c63be370a3fb92c89774f4a7e9683b882535
Kris Fernando <krisfernando6@gmail.com>
2024-07-29 11:43:50 -0400

    added auto play feature

--- ComputerHarware/js/speaker.js
+++ ComputerHarware/js/speaker.js
@@ -1,25 +1,29 @@
 let speech = new SpeechSynthesisUtterance();
 
+
 document.querySelector("#play").addEventListener("click", () => {
     window.speechSynthesis.cancel();
-    section = document.querySelector(".active").getAttribute("data-index");
-    console.log(section);
-    titles = document.querySelectorAll("h2");
-    paragraphs = document.querySelectorAll("p");
-    next = paragraphs[section].nextElementSibling;
-    listitems = next.querySelectorAll("li");
-    length = listitems.length;
-    console.log(listitems.length);
-    list = ""
-    listitems.forEach(item => list += item.innerHTML + "; ");
-    speech.text = (titles[section].innerHTML + ": " + paragraphs[section].innerHTML + " " + list);
-    console.log(speech.text);
-    window.speechSynthesis.speak(speech);
+    playSection(0);
 })
 
-document.querySelectorAll(".carousel-icon").forEach(button => 
-  button.addEventListener("click", () => window.speechSynthesis.cancel())
-)
+document.querySelector(".carousel-control-prev").addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+    autoplay = document.querySelector("#auto").checked;
+    console.log("auto: "+autoplay);
+    if (autoplay) {
+        playSection(-1);
+    }
+})
+
+document.querySelector(".carousel-control-next").addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+    autoplay = document.querySelector("#auto").checked;
+    console.log("auto: "+autoplay);
+    if (autoplay) {
+        playSection(1);
+    }
+})
+
 
 function removeTags(str) {
     if ((str === null) || (str === ''))
@@ -33,3 +37,22 @@ function removeTags(str) {
     return str.replace(/(<([^>]+)>)/ig, '');
 }
 
+function playSection(offset) {
+    let section = parseInt(document.querySelector(".active").getAttribute("data-index"))+parseInt(offset);
+    let titles = document.querySelectorAll("h2");
+    let paragraphs = document.querySelectorAll("p");
+    let next = paragraphs[section].nextElementSibling;
+    let listitems = next.querySelectorAll("li");
+    let length = listitems.length;
+    let list = ""
+
+    if (section == -1) {
+        section = titles.length;    
+    }
+    console.log("index: "+section);
+    console.log(listitems.length);
+    listitems.forEach(item => list += item.innerHTML + "; ");
+    speech.text = (titles[section].innerHTML + ": " + paragraphs[section].innerHTML + " " + list);
+    console.log(speech.text);
+    window.speechSynthesis.speak(speech);
+}
