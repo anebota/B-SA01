6c4fd425389f7217f138975a4be82860b121c270
Kris Fernando <krisfernando6@gmail.com>
2024-07-29 00:23:21 -0400

    added carousel and slides

--- ComputerHarware/js/speaker.js
+++ ComputerHarware/js/speaker.js
@@ -1,7 +1,24 @@
 let speech = new SpeechSynthesisUtterance();
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("#play").addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+    section = document.querySelector(".active").getAttribute("data-index");
+    console.log(section);
+    titles = document.querySelectorAll("h2");
     paragraphs = document.querySelectorAll("p");
-    speech.text = paragraphs[0].textContent;
+    speech.text = removeTags(titles[section].innerHTML + ": " + paragraphs[section].innerHTML);
+    console.log(speech.text);
     window.speechSynthesis.speak(speech);
 })
+
+function removeTags(str) {
+    if ((str === null) || (str === ''))
+        return false;
+    else
+        str = str.toString();
+
+    // Regular expression to identify HTML tags in
+    // the input string. Replacing the identified
+    // HTML tag with a null string.
+    return str.replace(/(<([^>]+)>)/ig, '');
+}
