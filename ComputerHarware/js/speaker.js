53381e0d028f9d0849e0cdebfa9c62d02a873dca
Kris Fernando <krisfernando6@gmail.com>
2024-07-30 10:37:49 -0400

    Interface upgrade

--- ComputerHarware/js/speaker.js
+++ ComputerHarware/js/speaker.js
@@ -1,5 +1,19 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let voiceSelect = document.querySelector("#voices")
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+}
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+})
 
 document.querySelector("#play").addEventListener("click", () => {
     window.speechSynthesis.cancel();
